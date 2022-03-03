b6addd3eba3725184dc81e658bee75943fda9708
Yeung Cheuk Long <clyeungai@connect.ust.hk>
2022-03-03 23:41:15 +0800

    Added package Jsoup

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,30 @@
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.Connection;
+import org.jsoup.Connection.Response;
+
+import java.io.IOException;
+
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Message from MainClass");
+        runParser();
     }
+
+    public static void runParser(){
+        final String url = "http://www.cse.ust.hk";
+        Connection conn = Jsoup.connect(url);
+        try {
+            Response res = conn.execute();
+            Boolean redirectFlag = res.hasHeader("location");
+            String redirectAddr = res.header("location");
+            String lastModified = res.header("last-modified");
+            System.out.println(redirectFlag);
+            System.out.println(redirectAddr);
+            System.out.println(lastModified);
+        } catch (HttpStatusException e) {
+        } catch (IOException e) {
+        }
+    }
+
 }
