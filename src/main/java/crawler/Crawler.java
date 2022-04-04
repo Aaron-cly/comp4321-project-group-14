fa835de2a9099d044279cc2369a2e4a421b27782
Aaron-cly <cheuklong.y@gmail.com>
2022-04-04 14:24:15 +0800

    Can crawl all page

--- src/main/java/crawler/Crawler.java
+++ src/main/java/crawler/Crawler.java
@@ -51,6 +51,17 @@ public class Crawler {
         return getPagesFromURL(url, Integer.MAX_VALUE);
     }
 
+    private static final List<String> extensionList = List.of(".pdf", "png", ".jpeg", ".jpg", ".mp4", ".mp3", ".doc", ".zip", ".rar", ".ppt", ".pptx", ".docx", ".bib", ".Z", ".ps", ".tgz");
+    private boolean validLink(String link) {
+
+        return !link.equals("/") && !link.startsWith("../") && !link.contains("ftp") && !link.contains("@") && !link.equals("index.html")
+                && !link.equals(".") && !link.contains("?") && !link.contains("#") && !link.contains("http")
+                && !link.startsWith("javascript")
+                && extensionList.stream().noneMatch(link::endsWith)
+                && extensionList.stream().map(String::toUpperCase).noneMatch(link::endsWith);
+
+    }
+
     // obtain all pages embedded in html of a URL
     private HashSet<String> getPagesFromURL(String url, int numPages) throws IOException {
         Document doc = null;
@@ -65,10 +76,33 @@ public class Crawler {
 
         Elements links = doc.select("a[href]"); // get all anchors with href attr
 
+        final String currentUrl = url;
         var urlSet = links.stream()
                 .map(link -> link.attr("href"))
-                .filter(link -> link.startsWith("https://cse.hkust.edu.hk/") || link.startsWith("/"))
-                .map(link -> link.charAt(0)=='/' ? rootURL+link : link)
+                .filter(this::validLink)
+                .map(link -> {
+                    if (link.startsWith("/")) {
+                        return rootURL + link;
+                    } else if (link.startsWith("./")){
+                        int mountPoint = currentUrl.lastIndexOf('/');
+                        String baseUrl = currentUrl.substring(0, mountPoint+1);
+
+                        return baseUrl + link.substring(2);
+                    } else {
+                        if (currentUrl.endsWith(".html")) {
+                            int mountPoint = currentUrl.lastIndexOf('/');
+                            String baseUrl = currentUrl.substring(0, mountPoint+1);
+                            return baseUrl + link;
+                        } else if (currentUrl.endsWith(".html/")) {
+                            int mountPoint = currentUrl.substring(0,currentUrl.length()-1).lastIndexOf('/');
+                            String baseUrl = currentUrl.substring(0, mountPoint+1);
+                            return baseUrl + link;
+                        } else {
+                            char lastChar = currentUrl.charAt(currentUrl.length()-1);
+                            return currentUrl + (lastChar=='/' ? "" : '/') + link;
+                        }
+                    }
+                })
                 .collect(Collectors.toCollection(HashSet::new));
 
         return urlSet.stream().limit(numPages).collect(Collectors.toCollection(HashSet::new));
@@ -82,6 +116,7 @@ public class Crawler {
     public void crawlFromRoot(int numPages) throws IOException {
         boolean isCrawlAll = numPages == VALUE_CRAWL_ALL;
         this.urlList.add(this.rootURL);
+        this.urlSet.add(this.rootURL);
 
         int currentIndex = 0;
         while (currentIndex < urlList.size() && (isCrawlAll || currentIndex < numPages)) {
@@ -94,7 +129,7 @@ public class Crawler {
                 }
             }
 
-//            crawlPage(currentURL);
+            crawlPage(currentURL, pagesOnURL);
             currentIndex++;
             if (currentIndex % 500 == 0) {
                 System.out.printf("Crawled and indexed %d pages\n", currentIndex);
@@ -102,7 +137,7 @@ public class Crawler {
         }
     }
 
-    private void crawlPage(String url) throws IOException {
+    private void crawlPage(String url, HashSet<String> pagesOnURL) throws IOException {
 
         Document doc = null;
         Connection.Response res = null;
@@ -114,7 +149,6 @@ public class Crawler {
         if (doc == null) return;
 
         String lastModifiedDate = getLastModifiedDate(res, doc);
-        var pagesOnURL = getPagesFromURL(url);
 
         // index the current page
         var map_word_posList = consolidatePositions(extractWords(doc));
