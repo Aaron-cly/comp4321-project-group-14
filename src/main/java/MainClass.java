5c598927d893c6b7aa585d8b70683e904befbc6d
Jedric01 <jedric@connect.ust.hk>
2022-04-27 17:03:51 +0800

    Added .wav to list of prohibited extensions

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -11,10 +11,11 @@ import java.time.Instant;
 
 public class MainClass {
     static String URL = "http://www.cse.ust.hk";
+//    static String URL = "http://www.cse.ust.hk/acm/photo/20040417HKUSTProgrammingContest/120_2020.wmv";
 
     public static void main(String[] args) throws RocksDBException, IOException {
-//        runCrawler();
-        runQuery("FAQ \"Postgraduate Students\"");
+        runCrawler();
+//        runQuery("FAQ \"Postgraduate Students\"");
 //        runQuery("Postgraduate");
     }
 
@@ -36,8 +37,8 @@ public class MainClass {
             Crawler crawler = new Crawler(URL);
             System.out.println("Running Crawler...");
             start = Instant.now();
-            crawler.crawlFromRoot(30);
-//            crawler.crawlFromRoot();
+//            crawler.crawlFromRoot(30);
+            crawler.crawlFromRoot();
             finish = Instant.now();
             timeElapsed = Duration.between(start, finish).toSeconds();
             System.out.println("Time elapsed crawling pages: " + timeElapsed + " seconds\n");
