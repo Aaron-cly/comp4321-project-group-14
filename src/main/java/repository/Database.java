7200c9ce6e977b3a456bf8d84dd5860279fff84d
Jedric01 <jedric@connect.ust.hk>
2022-04-29 01:28:18 +0800

    Change root directory if not running in JSP

--- src/main/java/repository/Database.java
+++ src/main/java/repository/Database.java
@@ -5,9 +5,17 @@ import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 
 public class Database {
-    public static String DB_dir = "./apache-tomcat-10.0.20/bin/rocksdb/";
+    // will only be appended to DB_dir if the classes are not running in JSP context
+    public static String prefix = "./apache-tomcat-10.0.20/bin";
+    public static String DB_dir = "./rocksdb/";
     private RocksDB DB;     // singleton
     private String fileName;
+
+    static{
+        var path = System.getProperty("user.dir");
+        if(!path.contains("apache"))
+            DB_dir = prefix + DB_dir.substring(1);
+    }
 
     public Database(String fileName) {
         this.fileName = fileName;
